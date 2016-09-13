857f65ce98d3dc42925d61331f5124467ea383d6
Лушковский Сергей <s.lushkovsky@gmail.com>
2016-09-14 01:15:09 +0300

    Both Node.js and browser

--- pyformat.js
+++ pyformat.js
@@ -1,42 +1,52 @@
-/*  Python new style srting formatter. 
- *
- *  Parameters
- *  ----------
- *  str   <String>  Format string
- *  data  <dict>    Data
- * 
- *  Return: <str>    
- */
+(function () {
+    if (typeof module !== 'undefined' && module.exports)  // node.js
+        module.exports = pyformat;
+    else                                                  // browser
+        window.pyformat = pyformat;
 
-function pyformat(str, data) {
-	assert(typeof str === 'string', 'Unexpected \'str\' argument type: ' + typeof str);
-	// assert typeof data == dict
 
-	for (var key in data) {
-		// assert(!'{' in key && !'}' in key && !'.' in key);
+    /*  Python new style srting formatter. 
+     *
+     *  Parameters
+     *  ----------
+     *  str   <String>  Format string
+     *  data  <dict>    Data
+     * 
+     *  Return: <str>    
+     */
 
-		var labelRe = new RegExp('\{' + key + '\.?([a-zA-Z.]+)?\}');
-		var matchedCount = 0;
+    function pyformat(str, data) {
+        if (typeof str !== 'string') {
+            console.error('Unexpected \'str\' argument type: ' + typeof str);
+            return;
+        }
 
-		while (true) {
-			var match = str.match(labelRe); 
+        // assert typeof data == dict
 
-			if (match === null) {
-				if (matchedCount == 0) 
-					console.log('Label \'' + key + '\' not found');
-				
-				break;
-			}
+        for (var key in data) {
+            // assert(!'{' in key && !'}' in key && !'.' in key);
 
-			matchedCount += 1;
+            var labelRe = new RegExp('\{' + key + '\.?([a-zA-Z.]+)?\}');
+            var matchedCount = 0;
 
-			var replaceOn = eval('data.' + key + (match[1] === undefined ? '' : '.' + match[1]));  
+            while (true) {
+                var match = str.match(labelRe); 
 
-			str = str.replace(match[0], String(replaceOn));
-		}
-	}
+                if (match === null) {
+                    if (matchedCount == 0) 
+                        console.log('Label \'' + key + '\' not found');
+                    
+                    break;
+                }
 
-	return str;
-}
+                matchedCount += 1;
 
-module.exports = pyformat
+                var replaceOn = eval('data.' + key + (match[1] === undefined ? '' : '.' + match[1]));  
+
+                str = str.replace(match[0], String(replaceOn));
+            }
+        }
+
+        return str;
+    }
+})();
